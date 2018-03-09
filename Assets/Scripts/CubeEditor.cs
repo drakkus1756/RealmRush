692ab1a107f36da5abb167e1b3ec7d0beb751bed
Drakkus <jebait@jebaited.com>
2018-03-09 01:04:19 -0500

    Organized a bit, nothing depends on CubeEditor, added pathfinder

---
+++ Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+[RequireComponent(typeof(Waypoint))]
+public class CubeEditor : MonoBehaviour {
+	
+    Waypoint waypoint;
+
+    void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+    }
+
+    void Update()
+    {
+        SnapToGrid();
+        UpdateLabel();
+    }
+
+    private void SnapToGrid()
+    {
+        Vector2 gridPos = waypoint.GetGridPos();
+
+        transform.position = new Vector3(gridPos.x, 0f, gridPos.y);
+    }
+
+    private void UpdateLabel()
+    {
+        int gridSize = waypoint.GetGridSize();
+
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
+        string labelText = waypoint.GetGridPos().x / gridSize + "," + waypoint.GetGridPos().y / gridSize;
+        textMesh.text = labelText;
+        gameObject.name = labelText;
+    }
+}
