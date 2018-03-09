e916cf0a85b248dfaa38b4939a5b1b1d728994a8
Drakkus <jebait@jebaited.com>
2018-03-09 12:16:30 -0500

    pathfinding is almost there

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -8,18 +8,88 @@ public class Pathfinder : MonoBehaviour {
 	[SerializeField] Waypoint startWaypoint, endWaypoint;
 
 	Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+	Queue<Waypoint> queue = new Queue<Waypoint>();
+	bool isRunning = true;
+
+	Vector2Int[] directions = {
+		Vector2Int.up,
+		Vector2Int.right,
+		Vector2Int.down,
+		Vector2Int.left };
 
 	// Use this for initialization
 	void Start () 
 	{
 		ColorWaypoints();
 		LoadBlocks();
+		Pathfind();
+		//ExploreNeighbors();
 	}
+
+    private void Pathfind()
+    {
+		queue.Enqueue(startWaypoint);
+
+		while(queue.Count > 0 && isRunning)
+		{
+			var searchCenter = queue.Dequeue();
+			searchCenter.isExplored = true;
+			print("Searching from " + searchCenter); // TODO: remove
+			StopIfEndFound(searchCenter);
+			ExploreNeighbors(searchCenter);
+		}
+		// TODO: workout path
+		print("Finished pathfinding?");
+    }
+
+    private void StopIfEndFound(Waypoint searchCenter)
+    {
+        if (searchCenter == endWaypoint)
+		{
+			print("Searching from end node, stopping search");
+			isRunning = false;
+		}
+    }
+
+    private void ExploreNeighbors(Waypoint from)
+    {
+		if (!isRunning) { return; }
+
+        foreach (Vector2Int direction in directions)
+		{
+			Vector2Int neighborCoords = from.GetGridPos() + direction;
+			try
+            {
+                QueueNewNeighbors(neighborCoords);
+            }
+            catch
+			{
+				// do nothing
+			}
+			
+		}
+    }
+
+    private void QueueNewNeighbors(Vector2Int neighborCoords)
+    {
+        Waypoint neighbor = grid[neighborCoords];
+		if (neighbor.isExplored)
+		{
+			// do nothing
+		}
+		else
+		{
+			neighbor.SetTopColor(Color.cyan); // TODO: move later
+        	queue.Enqueue(neighbor);
+        	print("Queuing " + neighbor);
+		}
+
+    }
 
     private void ColorWaypoints()
     {
         startWaypoint.SetTopColor(Color.green);
-		endWaypoint.SetTopColor(Color.blue);
+		endWaypoint.SetTopColor(Color.red);
     }
 
     private void LoadBlocks()
